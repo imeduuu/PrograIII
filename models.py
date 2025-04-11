81a8cb6444d7a7a8a9068424d2558351e3b2269e
imeduuu <128259631+imeduuu@users.noreply.github.com>
2025-04-11 19:35:44 -0400

    ya esta xdxdxd

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from database import Base
+from datetime import datetime
 
 class Personaje(Base):
     __tablename__ = "personajes"
@@ -9,15 +10,24 @@ class Personaje(Base):
     nombre = Column(String, index=True)
     xp = Column(Integer, default=0)
 
-    misiones = relationship("Mision", back_populates="personaje", cascade="all, delete-orphan")
+    misiones = relationship("PersonajeMision", back_populates="personaje", cascade="all, delete-orphan")
 
 class Mision(Base):
     __tablename__ = "misiones"
 
     id = Column(Integer, primary_key=True, index=True)
     descripcion = Column(String)
-    
     xp = Column(Integer)
 
+    personajes = relationship("PersonajeMision", back_populates="mision", cascade="all, delete-orphan")
+
+class PersonajeMision(Base):
+    __tablename__ = "personaje_mision"
+
+    id = Column(Integer, primary_key=True, index=True)
     personaje_id = Column(Integer, ForeignKey("personajes.id"))
+    mision_id = Column(Integer, ForeignKey("misiones.id"))
+    orden = Column(Integer) 
+
     personaje = relationship("Personaje", back_populates="misiones")
+    mision = relationship("Mision", back_populates="personajes")
